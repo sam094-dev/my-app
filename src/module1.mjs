0e545c2251e91ede55b6e6b54b70cbe8d45ed7e1
sam094-dev <saurabhgupta.knp2019@gmail.com>
2023-03-03 22:47:43 +0530

    vedio 6 completed

---
+++ src/module1.mjs
@@ -0,0 +1,8 @@
+var a = "SAURABH";
+var b = "shivam";
+var c = "Akash";
+
+
+export default c;
+export { a };
+export { b };
---
+++ src/module2.mjs
@@ -0,0 +1,4 @@
+import name, { a, b } from './module1.mjs'
+console.log(name);
+console.log(a);
+console.log(b);
