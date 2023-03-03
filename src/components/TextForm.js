7c4a0d4961f7d42c88320c3ef5ea8dcb0904e6d9
sam094-dev <saurabhgupta.knp2019@gmail.com>
2023-03-04 00:21:56 +0530

    Video 8 Completed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,19 +7,30 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLowClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     const handleOnChange = (event) => {
         setText(event.target.value);
     }
-    const [text, setText] = useState('Enter Text Here');
+    const [text, setText] = useState('');
     // text= "new Text"; // Wrong way to change the state
     //setText(" new Text");  // Correct Way to change the state
     return (
-        <div>
-            <div className="mb-3">
-                <h1>{props.heading} </h1>
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
+        <>
+            <div className="container">
+                <div className="mb-3">
+                    <h1>{props.heading} </h1>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
+                </div>
+                <button className="btn.btn-primary mx-2" onClick={handleUpClick} >Convert to upper case</button>
+                <button className="btn.btn-primary mx-2" onClick={handleLowClick} >Convert to lower case</button>
             </div>
-            <button className="btn.btn-primary" onClick={handleUpClick}>Convert to upper case</button>
-        </div>
+            <div className="container my-4">
+                <h1>Your Text Here</h1>
+                <p>{text.split(" ").length} Words and {text.length} Charecter</p>
+            </div>
+        </>
     )
 }
